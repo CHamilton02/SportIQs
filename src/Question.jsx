41faf9e338e70e6114ffab34eec1f127306fd908
Camdyn Hamilton <c02hamil@gmail.com>
2024-07-11 14:08:36 -0400

    Implemented additional code to handle showing correct answer even when user didn't select it.

--- src/Question.jsx
+++ src/Question.jsx
@@ -11,22 +11,58 @@ export default function Question(props) {
                 props.gameFinished ?
                 <div className="button-container">
                     <button
-                        className={props.selectedElement === 0 ? 0 === props.correctLocation ? "correct-answer" : "wrong-answer inactive" : "unselected-button inactive"}
+                        className={props.selectedElement === 0 ?
+                            0 === props.correctLocation ?
+                                "correct-answer"
+                                :
+                                "wrong-answer inactive"
+                            :
+                            0 === props.correctLocation ? "correct-answer inactive"
+                                :
+                                "unselected-button inactive"
+                        }
                     >
                         {decode(props.options[0])}
                     </button>
                     <button
-                        className={props.selectedElement === 1 ? 1 === props.correctLocation ? "correct-answer" : "wrong-answer inactive" : "unselected-button inactive"}
+                        className={props.selectedElement === 1 ?
+                            1 === props.correctLocation ?
+                                "correct-answer"
+                                :
+                                "wrong-answer inactive"
+                            : 1 === props.correctLocation ?
+                                "correct-answer inactive"
+                                :
+                                "unselected-button inactive"
+                        }
                     >
                         {decode(props.options[1])}
                     </button>
                     <button
-                        className={props.selectedElement === 2 ? 2 === props.correctLocation ? "correct-answer" : "wrong-answer inactive" : "unselected-button inactive"}
+                        className={props.selectedElement === 2 ?
+                            2 === props.correctLocation ?
+                                "correct-answer"
+                                :
+                                "wrong-answer inactive"
+                            :
+                            2 === props.correctLocation ? "correct-answer inactive"
+                                :
+                                "unselected-button inactive"
+                        }
                     >
                         {decode(props.options[2])}
                     </button>
                     <button
-                        className={props.selectedElement === 3 ? 3 === props.correctLocation ? "correct-answer" : "wrong-answer inactive" : "unselected-button inactive"}
+                        className={props.selectedElement === 3 ?
+                            3 === props.correctLocation ?
+                                "correct-answer"
+                                :
+                                "wrong-answer inactive"
+                            :
+                            3 === props.correctLocation ? "correct-answer inactive"
+                                :
+                                "unselected-button inactive"
+                        }
                     >
                         {decode(props.options[3])}
                     </button>
